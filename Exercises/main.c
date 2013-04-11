1ecd0edbea11c3005b3b4e242eccfa29c8df9e40
github@profics.ch <ls@feanor.profics.local>
2013-04-11 10:59:47 +0200

    Initial Commit

---
+++ Exercises/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Exercises
+//
+//  Created by Lars Soltau on 11.04.13.
+//  Copyright (c) 2013 Profics. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
